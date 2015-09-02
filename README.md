48fd1c96d7095627056b50e681824a3d40bf669a
Jacques Bachelier <jacbachelier@gmail.com>
2015-09-03 01:44:02 +0200

    Refonte website v2.0

--- README.md
+++ README.md
@@ -1,27 +1,42 @@
 # NantesJS.org
 
-This is the source of the nantesjs.org website. Fork it to propose enhancements, as it is in a "gh-pages" branch, you can show the result of your version to anyone via URLs formed like this:
+This is the source of the [nantesjs.org](http://nantesjs.org) website.
 
-http://{Your_GitHub_Username}.github.com/nantesjs-website/
+Fork it to propose enhancements, as it is in a `gh-pages` branch, you can show the result of your version to anyone via URLs formed like this:
 
-the version on this repository can then be tested on http://nantesjs.github.com/nantesjs-website/
+    http://{Your_GitHub_Username}.github.com/nantesjs-website/
 
-For anything but little fixes, please propose your modifications on the mailing list before sending a Pull Request so all the community can agree on it: http://groups.google.com/group/nantesjs/
+The version on this repository can then be tested on [nantesjs.github.com/nantesjs-website](http://nantesjs.github.com/nantesjs-website/).
+
+For anything but little fixes, please propose your modifications on the mailing list before sending a `Pull Request` so all the community can agree on it: [groups.google.com/group/nantesjs](http://groups.google.com/group/nantesjs/)
+
 Note that any updates done to this website will available under the license given bellow.
 
+## Installation for development
+
+```shell
+cd /home/your_user/your_workspace
+git clone ...
+cd nantesjs-website
+[sudo] npm install gulp -g
+[sudo] npm install
+gulp
+```
+
+That's it.
 
 ## License
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                    Version 2, December 2004
+                DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                        Version 2, December 2004
 
-    Copyright (C) 2010-2011 the nantesjs community <http://nantesjs.org>
+    Copyright (C) 2010-2015 the NantesJS community <http://nantesjs.org>
 
     Everyone is permitted to copy and distribute verbatim or modified
     copies of this license document, and changing it is allowed as long
     as the name is changed.
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
     TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
     0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ gulpfile.js
@@ -0,0 +1,170 @@
+var gulp         = require('gulp');
+var notify       = require('gulp-notify');
+var plumber      = require('gulp-plumber');
+var rename       = require('gulp-rename');
+var browserSync  = require('browser-sync').create();
+
+var sass         = require('gulp-sass');
+var uglify       = require('gulp-uglify');
+var minifyCss    = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps   = require('gulp-sourcemaps');
+
+var jpegoptim    = require('imagemin-jpegoptim');
+var pngquant     = require('imagemin-pngquant');
+var optipng      = require('imagemin-optipng');
+var svgo         = require('imagemin-svgo');
+
+/* ========================================================================== */
+/* CONFIG HELPER                                                              */
+/* ========================================================================== */
+
+var path = {
+    src: {
+        dir: 'src/',
+        styles: 'styles/**.*',
+        scripts: 'scripts/**.*',
+        images: 'images/**/*.{png,jpg,jpeg,gif,svg}',
+        files: '*.html'
+    },
+    build: {
+        dir: 'build/',
+        styles: 'styles/',
+        scripts: 'scripts/',
+        images: 'images/'
+    }
+};
+
+var onError = function(err) {
+    notify.onError({
+        title:    'Gulp',
+        subtitle: 'Failure!',
+        message:  'Error: <%= error.message %>',
+        // sound:    'Beep'
+    })(err);
+
+    this.emit('end');
+};
+
+/* ========================================================================== */
+/* BANNER HELPER                                                              */
+/* ========================================================================== */
+
+// Load the package.json and prepare banner
+var pkg = require('./package.json');
+var year = new Date().getFullYear();
+var banner = ['/**',
+  ' * <%= pkg.owner %>',
+  ' *',
+  ' * @link    <%= pkg.homepage %>',
+  ' * @version v<%= pkg.version %> (<%= year %>)',
+  ' * @author  <%= pkg.author.name %> => <%= pkg.author.home %> && <%= pkg.author.twitter %>',
+  ' *',
+  ' * Copyright (c) <%= year %> ' + '<%= pkg.owner %>',
+  ' */',
+  ''].join('\n');
+
+/* ========================================================================== */
+/* DEFAULT TASK                                                               */
+/* ========================================================================== */
+
+// Static Server + watching scss/js/html files
+gulp.task('default', ['serve']);
+
+gulp.task('serve', ['styles', 'scripts', 'files', 'images'], function() {
+    browserSync.init({
+        proxy: 'nantesjs'
+    });
+
+    gulp.start('watchers');
+});
+
+/* ========================================================================== */
+/* GENERAL TASKS                                                              */
+/* ========================================================================== */
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('styles', function() {
+    return gulp.src(path.src.dir + path.src.styles)
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            sourcemap: true
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(minifyCss())
+        .pipe(rename({ extname: '.min.css'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.dir + path.build.styles))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(path.src.dir + path.src.scripts)
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(uglify({
+            mangle: true,
+            preserveComments: false
+        }))
+        .pipe(rename({ extname: '.min.js'}))
+        .pipe(gulp.dest(path.build.dir + path.build.scripts));
+});
+
+gulp.task('files', function() {
+    return gulp.src(path.src.dir + path.src.files)
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(gulp.dest(path.build.dir));
+});
+
+gulp.task('images', function() {
+    return gulp.src(path.src.dir + path.src.images)
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(pngquant({
+            quality: '65-80',
+            speed: 4
+        })())
+        .pipe(optipng({optimizationLevel: 3})())
+        .pipe(jpegoptim({
+            max: 70
+        })())
+        .pipe(svgo()())
+        .pipe(gulp.dest(path.build.dir + path.build.images));
+});
+
+/* ========================================================================== */
+/* WATCHER TASK                                                               */
+/* ========================================================================== */
+
+gulp.task('watchers', function() {
+  var watcherStyles = gulp.watch('src/styles/partials/**.scss', ['styles']);
+  watcherStyles.on('change', function (event) {
+    console.log('Event type: ' + event.type);
+    console.log('Event path: ' + event.path);
+  });
+
+  var watcherScripts = gulp.watch('src/scripts/**.*', ['scripts-watch']);
+  watcherScripts.on('change', function (event) {
+    console.log('Event type: ' + event.type);
+    console.log('Event path: ' + event.path);
+  });
+
+  var watcherImages = gulp.watch('src/images/**.*', ['images']);
+  watcherImages.on('change', function (event) {
+    console.log('Event type: ' + event.type);
+    console.log('Event path: ' + event.path);
+  });
+
+  var watcherFiles = gulp.watch('src/*.html', ['files']);
+  watcherFiles.on('change', function (event) {
+    console.log('Event type: ' + event.type);
+    console.log('Event path: ' + event.path);
+  });
+
+  gulp.watch('build/*.html').on('change', browserSync.reload);
+});
+
+// create a task that ensures the `scripts` task is complete before reloading browsers
+gulp.task('scripts-watch', ['scripts'], browserSync.reload);
