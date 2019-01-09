642b40b3c3623d0eb61b3f883aa6676413452255
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-01-09 08:52:48 +0100

    :fire: Remove k-ramel for data

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import Layout from '../components/layout'
 import { NextMeetup } from '../components/NextMeetup'
 import { OldMeetups } from '../components/OldMeetups'
 
-import styles from './index.module.css'
-
 function IndexPage () {
   return (
     <Layout>
