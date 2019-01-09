dd8bc1b40f01e32ff133e73c5160b6d7ad229773
Charles-Henri GUÉRIN <charleshenri.guerin@icloud.com>
2019-01-09 20:49:32 +0100

    :sparkles: Introduce PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,21 @@ const options = {
         path:`${ __dirname }/datas/meetups`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'NantesJS',
+        short_name: 'NantesJS',
+        start_url: '/',
+        background_color: `#f7f0eb`,
+        theme_color: '#a2466c',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
   ]
 }
 
