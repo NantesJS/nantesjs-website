b7a18134722d679ef92d02df8ae34f21e589f6be
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-01-10 09:36:44 +0100

    :sparkles: Add sponsors section

---
+++ src/components/Sponsors/Sponsors.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+import { withPrefix } from 'gatsby'
+
+import { FullWidthContainer } from '../FullWidthContainer'
+
+import styles from './Sponsors.module.css'
+
+export function Sponsors () {
+  return (
+    <Fragment>
+      <FullWidthContainer className={styles.sponsors__hero}>
+        <h3 className={styles.sponsors__title}>Sponsors 2019</h3>
+      </FullWidthContainer>
+      <div className={styles.sponsors__logos}>
+        <img
+          className={styles.sponsors__logo}
+          src={withPrefix('/images/sponsor/alakarteio.png')}
+        />
+      </div>
+    </Fragment>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Layout from '../components/layout'
 import { NextMeetup } from '../components/NextMeetup'
 import { OldMeetups } from '../components/OldMeetups'
+import { Sponsors } from '../components/Sponsors'
 
 function IndexPage () {
   return (
     <Layout>
       <NextMeetup />
+      <Sponsors />
       <OldMeetups />
     </Layout>
   )
