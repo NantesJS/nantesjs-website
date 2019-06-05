dafac3ed5ec9af12da6ebdad5690d0a9ebd75640
greizgh <greizgh@ephax.org>
2019-06-05 08:28:15 +0200

    :pager: Expose RSS feed (#105)

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,32 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const {
+  createFilePath
+} = require(`gatsby-source-filesystem`)
+
 // You can delete this file if you're not using it
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+
+exports.onCreateNode = ({
+  node,
+  actions,
+  getNode
+}) => {
+  const {
+    createNodeField
+  } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({
+      node,
+      getNode
+    })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
