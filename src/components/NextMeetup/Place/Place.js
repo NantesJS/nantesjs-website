82a712fa73a3e547673cd36b580ac21e2d5d6b47
Jules LE BRIS <jls.lebris@gmail.com>
2019-06-08 20:56:17 +0200

    change map w/ leaflet

--- src/components/NextMeetup/Place/Place.js
+++ src/components/NextMeetup/Place/Place.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { number } from 'prop-types'
 import { FullWidthContainer } from '../../FullWidthContainer'
-import ReactMapGL, { Marker } from 'react-map-gl'
 import { withPrefix } from 'gatsby'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import styles from './Place.module.css'
-
-import 'mapbox-gl/dist/mapbox-gl.css'
-
-const accessToken = process.env.GATSBY_MAPBOX_TOKEN
 
 Place.propTypes = {
   latitude: number,
@@ -16,29 +12,25 @@ Place.propTypes = {
 }
 
 export function Place ({ latitude, longitude }) {
-  if (!latitude || !longitude || !accessToken) return null
+  if (!latitude || !longitude ) return null
 
-  const coordinates = { longitude, latitude }
+  const position = [latitude, longitude]
 
   return (
     <FullWidthContainer className={styles.place}>
       <div className={styles.place__map}>
         {typeof window !== 'undefined' && (
-          <ReactMapGL
-            mapboxApiAccessToken={accessToken}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-            {...coordinates}
-            zoom={16}
-            width="100%"
-            height="100%"
-          >
-            <Marker {...coordinates} offsetLeft={-25} offsetTop={-25}>
-              <img
-                src={withPrefix('/images/logotype.png')}
-                height="50px"
-              />
+          <Map center={position} zoom={13} className={styles.leaflet_container}>
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
             </Marker>
-          </ReactMapGL>
+          </Map>
         )}
       </div>
     </FullWidthContainer>
