f612d5b7485d3aa01cc5984d6a6d1caf2b045256
Martin Klauer <mdklauer@gmail.com>
2020-01-02 11:20:40 +0100

    :construction: Add picture for participation and add a conditional view

--- src/pages/profil.js
+++ src/pages/profil.js
@@ -4,6 +4,8 @@ import styles from './profil.module.css';
 import QRCode from '../../static/images/QRCode.png';
 import Fusee from '../../static/images/Fusee.png';
 import Smiley from '../../static/images/Smiley.png';
+import VotingOK from '../../static/images/VotingOK.png';
+import VotingNO from '../../static/images/VotingNO.png';
 import { FullWidthContainer } from '../components/FullWidthContainer';
 import QrReader from 'react-qr-reader';
 
@@ -17,42 +19,79 @@ export default function Profil() {
   };
 
   const handleScan = data => {
-    if(data){
+    if (data) {
       setResult(data)
     }
   };
 
   return (
     <div className={styles.profilPage}>
-      <div className={styles.profilPage__ImageAndName}>
-        <h1>Mon profil</h1>
-        <p>J'ai participé à XX NantesJS au cours de l'année 2020</p>
-      </div>
-
-      <FullWidthContainer>
-        <div className={styles.profilPage__QRCodeDiv}>
-          <div className={isHere ? styles.QRCodeDiv__QRCodeImageOff : styles.QRCodeDiv__QRCodeImageOn}>
-            <img src={QRCode} alt="QRCode" className={styles.QRCodeDiv__QRCodeImage} />
-            <button className={styles.QRCodeDiv__QRCodeButton} onClick={handleClick}>Scanner un QRCode</button>
+      {result == "Nothing" ?
+        <div>
+          <div className={styles.profilPage__ImageAndName}>
+            <h1>Mon profil</h1>
+            <p>J'ai participé à XX NantesJS au cours de l'année 2020</p>
           </div>
-          <div className={isHere ? styles.QRCodeDiv__cameraOn : styles.QRCodeDiv__cameraOff}>  
-            <QrReader 
-              delay={300}
-              onError={(error) => {console.log(error)}}
-              onScan={handleScan}
-            />
-            <p>{result}</p>
-          </div>
-          <div className={styles.QRCodeDiv__fusee}>
-            <img src={Fusee} alt="Fusee" className={styles.fusee__fuseeImage} />
-            <button className={styles.fusee__badgesButton} >Voir mes badges</button>
-          </div>
-          <div className={styles.QRCodeDiv__smiley}>
-            <img src={Smiley} alt="Smiley" className={styles.smiley__smileyImage} />
-            <button className={styles.smiley__badgesButton} >Mon profil public</button>
-          </div>
+          <FullWidthContainer>
+            <div className={styles.profilPage__QRCodeDiv}>
+              <div className={isHere ? styles.QRCodeDiv__QRCodeImageOff : styles.QRCodeDiv__QRCodeImageOn}>
+                <img src={QRCode} alt="QRCode" className={styles.QRCodeDiv__QRCodeImage} />
+                <button className={styles.QRCodeDiv__QRCodeButton} onClick={handleClick}>Scanner un QRCode</button>
+              </div>
+              <div className={isHere ? styles.QRCodeDiv__cameraOn : styles.QRCodeDiv__cameraOff}>
+                <QrReader
+                  delay={300}
+                  onError={(error) => { console.log(error) }}
+                  onScan={handleScan}
+                />
+                <button onClick={handleClick} className={styles.QRCodeDiv__QRCodeButton}>Retour</button>
+              </div>
+              <div className={styles.QRCodeDiv__fusee}>
+                <img src={Fusee} alt="Fusee" className={styles.fusee__fuseeImage} />
+                <button className={styles.fusee__badgesButton} >Voir mes badges</button>
+              </div>
+              <div className={styles.QRCodeDiv__smiley}>
+                <img src={Smiley} alt="Smiley" className={styles.smiley__smileyImage} />
+                <button className={styles.smiley__badgesButton} >Mon profil public</button>
+              </div>
+            </div>
+          </FullWidthContainer>
         </div>
-      </FullWidthContainer>
+        :
+        <div>
+          {result == "MEETUP" ?
+            <div>
+              <div className={styles.profilPage__ImageAndName}>
+                <h1>Mon profil</h1>
+                <p>Votre participation au NantesJS XX a bien été enregistrée !</p>
+              </div>
+              <FullWidthContainer>
+                <div className={styles.profilPage__QRCodeDiv}>
+                  <div className={styles.QRCodeDiv__QRCodeImageOn}>
+                    <img src={VotingOK} alt="QRCode" className={styles.QRCodeDiv__QRCodeImage} />
+                    <button className={styles.QRCodeDiv__QRCodeButton} onClick={handleClick}>Revenir à mon profil</button>
+                  </div>
+                </div>
+              </FullWidthContainer>
+            </div>
+            :
+            <div>
+              <div className={styles.profilPage__ImageAndName}>
+                <h1>Mon profil</h1>
+                <p>Vous avez déjà enregistré votre participation pour ce NantesJS...</p>
+              </div>
+              <FullWidthContainer>
+                <div className={styles.profilPage__QRCodeDiv}>
+                  <div className={styles.QRCodeDiv__QRCodeImageOn}>
+                    <img src={VotingNO} alt="QRCode" className={styles.QRCodeDiv__QRCodeImage} />
+                    <button className={styles.QRCodeDiv__QRCodeButton} onClick={handleClick}>Revenir à mon profil</button>
+                  </div>
+                </div>
+              </FullWidthContainer>
+            </div>
+            }
+        </div>
+      }
     </div>
   )
 }
