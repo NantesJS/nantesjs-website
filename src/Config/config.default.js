d6ee99dfcaad36e158b3c0cbbd8de818a37b29e5
Bdecline <bdeglane@users.noreply.github.com>
2020-03-03 10:35:38 +0100

    :rotating_light: Removing linter warnings

--- src/Config/config.default.js
+++ src/Config/config.default.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 
 export default firebase.initializeApp({
   apiKey: "[YOUR_API_KEY]",
   authDomain: "[YOUR_AUTH_DOMAIN].firebaseapp.com",
   projectId: "[YOUR_PROJECT_ID]",
-});
+})
