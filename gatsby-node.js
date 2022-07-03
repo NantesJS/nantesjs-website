1386f73c85e20051d4a0c0bf10f8d5ae99fe44e6
Yvonnick Frin <frin.yvonnick@gmail.com>
2022-07-03 18:49:35 +0200

    :sparkles: Add poster generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,21 @@
+require("dotenv").config({
+  path: `.env.${ process.env.NODE_ENV }`,
+})
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-const {
-  createFilePath
-} = require(`gatsby-source-filesystem`)
-
-// You can delete this file if you're not using it
-require("dotenv").config({
-  path: `.env.${ process.env.NODE_ENV }`,
-})
-
+const fs = require('fs')
 const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const nodeHtmlToImage = require('node-html-to-image')
 const { parse } = require('date-fns/fp')
-
+const { format } = require('date-fns')
+const { fr } = require('date-fns/locale')
+const smartTruncate = require('smart-truncate');
+const he = require('he')
 const parseDate = parse(new Date(), 'dd/MM/yyyy')
 
 exports.onCreateNode = ({
@@ -43,7 +43,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
     query {
-      allMarkdownRemark(filter: { frontmatter: { status: { eq: "done" } } }) {
+      allMarkdownRemark {
         edges {
           node {
             parent  {
@@ -66,6 +66,7 @@ exports.createPages = async ({ graphql, actions }) => {
               talks {
                 id
                 title
+                description
                 video
                 speakers {
                   id
@@ -79,7 +80,68 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
+  
+  createMeetupsByYearPage(result, { createPage })
 
+  await createMeetupPosters(result)
+}
+
+async function createMeetupPosters (result) {
+  console.log('⏳ Start creating posters')
+  const meetups = result.data.allMarkdownRemark.edges.map(({ node }) => {
+    return {
+      ...node.frontmatter,
+      filename: node.parent.name
+    }
+  })
+
+  const postersToCreate = meetups.filter(({ filename }) => {
+    return !fs.existsSync(path.join(__dirname, 'static', 'posters', `${ filename }-poster.jpg`))
+  })
+  console.log('🚀 Posters to create :', postersToCreate.length)
+  
+  await nodeHtmlToImage({
+    html: fs.readFileSync(path.join(__dirname, 'src', 'templates', 'meetup-poster.html')).toString('utf8'),
+    content: postersToCreate.map(meetup => ({
+      output: path.join(__dirname, 'static', 'posters', `${ meetup.filename }-poster.jpg`),
+      title: meetup.title,
+      day: format(parseDate(meetup.date), 'dd', { locale: fr }),
+      month: format(parseDate(meetup.date), 'MMM', { locale: fr }),
+      sponsor: meetup.sponsor ? meetup.sponsor.name : 'NantesJS',
+      venue: meetup.venue ? meetup.venue.name : '',
+      ticketsUrl: meetup.ticketsUrl,
+      talks: meetup.talks.map(talk => {
+        talk.description = talk.description ? smartTruncate(he.decode(talk.description), 300) : ''
+        talk.title = he.decode(talk.title)
+        talk.speakers = talk.speakers.map((speaker, index) => {
+          // Gérer pas de link
+          if (speaker.link) {
+            const linkParts = speaker.link.split('/')
+            speaker.handle = linkParts.pop()
+          } 
+
+          if (talk.speakers.length > 1) {
+            const minSize = 25
+            const variableSize = 75 / talk.speakers.length
+            const percent = `${ minSize + variableSize }%`
+            // Problème de positionnement en hauteur
+            const position = `${ index * variableSize }%`
+
+            // eslint-disable-next-line max-len
+            speaker.style = `width: ${ percent }; padding-top: ${ percent }; border-radius: ${ percent }; position: absolute; top: ${ position }; left: ${ position }`
+          }
+
+          return speaker
+        })
+        talk.twitterHandles = talk.speakers.map(s => s.handle).join(', ')
+        return talk
+      }),
+    }))
+  })
+  console.log('✅ Posters successfully created')
+}
+
+function createMeetupsByYearPage (result, { createPage }) {
   const currentYear = new Date().getFullYear()
 
   const meetupsByYear = result.data.allMarkdownRemark.edges.reduce((acc, { node }) => {
