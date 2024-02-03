50864671f4bec732d826e24a96d62522772a1d01
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2024-02-03 23:30:55 +0100

    :sparkles: Add NavLink component

---
+++ src/app/components/NavLink/NavLink.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { useSelectedLayoutSegment } from 'next/navigation'
+
+export function NavLink({ slug = null, children }) {
+    const segment = useSelectedLayoutSegment()
+    const isActive = slug === segment ?? slug === null
+
+    return (
+        <Link
+            href={`/${slug ?? ''}`}
+            className={isActive ? 'active' : null}
+        >
+            {children}
+        </Link>
+    )
+}
