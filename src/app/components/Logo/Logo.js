7072b6b56ec3caca5481ac7fe5e91daa14b9fe02
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2024-02-03 23:30:39 +0100

    :sparkles: Add Logo component

--- src/app/components/Logo/Logo.js
+++ src/app/components/Logo/Logo.js
@@ -7,6 +7,7 @@ export function Logo() {
             alt="Logo de NantesJS - Communauté nantaise des utilisateurs de JavaScript"
             height={500}
             width={500}
+            className='logo'
         />
     )
 }
