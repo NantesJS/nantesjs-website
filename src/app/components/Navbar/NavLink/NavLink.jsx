c17f477f8b6556b1f91705b9cb6c4463189caad6
mcampourcy <mireille.campourcy@gmail.com>
2024-02-06 10:39:50 +0100

    :recycle: tech: Fix lint errors

--- src/app/components/Navbar/NavLink/NavLink.jsx
+++ src/app/components/Navbar/NavLink/NavLink.jsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
-import { useSelectedLayoutSegment } from 'next/navigation'
+import { useSelectedLayoutSegments } from 'next/navigation'
 
-export function NavLink ({ slug = null, children }) {
-    const segment = useSelectedLayoutSegment()
-    const isActive = slug === segment ?? slug === null
+export function NavLink ({ active = false, children, slug }) {
+    const segmentList = useSelectedLayoutSegments()
+    const [segment] = segmentList.filter((el) => !el.match(/[()]/gm))
+    const isHomepage = !slug && segmentList.length === 0
+
+    const isActive = slug && slug === segment || isHomepage
 
     return (
         <Link href={`/${slug ?? ''}`} className={isActive ? 'active' : null}>
