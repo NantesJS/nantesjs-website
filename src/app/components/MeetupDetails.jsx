0d15d6a83b022aeb1aa67d87c394c37a7f6b7f60
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2024-11-24 22:23:44 +0100

    ♻️ Improve Hero meetup display

--- src/app/components/MeetupDetails.jsx
+++ src/app/components/MeetupDetails.jsx
@@ -3,22 +3,18 @@ import { MainSection } from '@/app/(layout)'
 import './MeetupDetails.css'
 
 export function MeetupDetails ({ meetup }) {
+    const isMeetupHero = meetup.type === 'meetup-hero'
+
     return (
         <MainSection className="meetup-details">
             {meetup.talks.map((talk, index) => (
                 <article key={index}>
-                    <h2>{talk.title}</h2>
-                    <p>{talk.description}</p>
-                    {talk.requirements && (
-                        <>
-                            <h3>Prérequis</h3>
-                            <ul>
-                                {talk.requirements.map((requirement) => (
-                                    <li key={requirement}>{requirement}</li>
-                                ))}
-                            </ul>
-                        </>
-                    )}
+                    {talk.title && <h2>{talk.title}</h2>}
+                    {isMeetupHero
+                        ? <MeetupHeroDescription description={talk.description} final={talk.final} />
+                        : <p>{talk.description}</p>
+                    }
+                    {talk.requirements && <TalkRequirements requirements={talk.requirements} />}
                     <h4>Speakers</h4>
                     {talk.speakers.map((speaker) => (
                         <p key={speaker.id}>{speaker.name}</p>
@@ -28,3 +24,27 @@ export function MeetupDetails ({ meetup }) {
         </MainSection>
     )
 }
+
+const MeetupHeroDescription = ({ description, final }) => (
+    <>
+        <ul>
+            {description.map(({ label, details }) => (
+                <li key={label}>
+                    <strong>{label}</strong> - {details}
+                </li>
+            ))}
+        </ul>
+        <p>{final}</p>
+    </>
+)
+
+const TalkRequirements = ({ requirements }) => (
+    <>
+        <h3>Prérequis</h3>
+        <ul>
+            {requirements.map((requirement) => (
+                <li key={requirement}>{requirement}</li>
+            ))}
+        </ul>
+    </>
+)
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-import { MeetupTitle } from '@/app/components'
+import { MeetupDetails, MeetupTitle } from '@/app/components'
 import { getSortedMeetupListByYear } from '@/lib'
 
 export default function Page () {
     const [ nextMeetup, ...meetupList ] = getSortedMeetupListByYear()
 
     return (
-        <MeetupTitle meetup={nextMeetup} />
+        <>
+            <MeetupTitle meetup={nextMeetup} />
+            <MeetupDetails meetup={nextMeetup} />
+        </>
     )
 }
 
