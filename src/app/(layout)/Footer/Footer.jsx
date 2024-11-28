67f148f5391e278af1e4fbc46725ef2362632abb
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2024-11-28 13:49:52 +0100

    ✨ Add footer

---
+++ src/app/(layout)/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Heart } from 'react-feather'
+import { Logo } from '@/app/components'
+
+import './Footer.css'
+
+export function Footer () {
+    return (
+        <footer>
+            <Logo />
+            <p>
+                <Heart />
+                <span>
+                    <Link href="/">NantesJS</Link>&nbsp;- © 2011-2024
+                </span>
+                <a href="https://www.netlify.com">
+                    <Image
+                        src="https://www.netlify.com/img/global/badges/netlify-light.svg"
+                        alt="Deploys by Netlify"
+                        width={110}
+                        height={50}
+                    />
+                </a>
+            </p>
+        </footer>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,20 @@
-import { Header } from '@/app/(layout)'
+import { Footer, Header } from '@/app/(layout)'
 import { firaSans, montserrat } from './fonts'
 import './globals.css'
 
 export const metadata = {
     title: 'NantesJS - Communauté nantaise des utilisateurs de JavaScript',
     description: 'Communauté Nantaise des utilisateurs de JavaScript',
-    keywords: 'meetup, Nantes, JavaScript',
+    keywords: 'meetup, Nantes, JavaScript'
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
     return (
         <html lang="fr">
             <body className={`${montserrat.className} ${firaSans.variable}`}>
                 <Header />
                 <main>{children}</main>
+                <Footer />
             </body>
         </html>
     )
