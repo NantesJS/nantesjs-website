010dc774f67c6b8c59f0df9cc0858ed7d28f1f81
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2024-12-03 21:17:26 +0100

    Update eslint and remove stylistic

--- src/app/(layout)/Header/SocialLink/socialLinksList.js
+++ src/app/(layout)/Header/SocialLink/socialLinksList.js
@@ -2,7 +2,6 @@ import { GitHub, Rss, Slack, Twitch, Twitter, Youtube } from 'react-feather'
 
 export const socialLinksList = [
     {
-        // eslint-disable-next-line @stylistic/max-len
         href: 'https://join.slack.com/t/nantesjs/shared_invite/enQtNTM0MjA0NTE0NTkzLTc3YWRkMjk3MTg3ZjEwYzVhYTA1YTA1OGE3MTdkMGE2ZjgzNzE1YzMwYWQwYmU0ZTEzNDhkZWUwZGY2ZjQ5OWY',
         label: 'Slack',
         icon: <Slack />
